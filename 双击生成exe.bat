932afe54650a0c2633bda572f76cdc4223b25e3f
junye.zheng <zhengjunye123@126.com>
2024-09-09 15:28:23 +0800

    改个名字

--- 双击生成exe.bat
+++ 双击生成exe.bat
@@ -1,6 +1,6 @@
 rmdir /s /q dist
 rmdir /s /q build
-pyinstaller -F -w .\zlgcan_demo.py
+pyinstaller -F -w .\dh_can_test.py
 copy .\*dll dist
 copy .\dev_info.json dist
 xcopy /E /I kerneldlls dist\kerneldlls 
